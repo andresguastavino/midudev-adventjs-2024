de002db15a1d7febffb5cb3759e3774321ce8374
andresguastavino <andres.guastavino@gmail.com>
2024-12-03 21:21:15 -0300

    1st day

---
+++ 1.js
@@ -0,0 +1,17 @@
+function prepareGifts(gifts) {
+  return gifts.filter((gift, index) => !gifts.slice(0, index).includes(gift))
+    .sort((a, b) => a - b)
+}
+
+const gifts1 = [3, 1, 2, 3, 4, 2, 5]
+const preparedGifts1 = prepareGifts(gifts1)
+console.log(preparedGifts1) // [1, 2, 3, 4, 5]
+
+const gifts2 = [6, 5, 5, 5, 5]
+const preparedGifts2 = prepareGifts(gifts2)
+console.log(preparedGifts2) // [5, 6]
+
+const gifts3 = []
+const preparedGifts3 = prepareGifts(gifts3)
+console.log(preparedGifts3) // []
+// There are no gifts, the list remains empty
